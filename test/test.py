7799f81e69238b89be29d70aff266112150775c7
Martin Lindberg <Martinlindberg@live.se>
2023-10-16 22:47:33 +0200

    update 231016

--- test/test.py
+++ test/test.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
+page = """
+    <html>
+    <head><title>Hem</title></head>
+    <body>
+    <h1>Titel</h1>
+    <p>Detta är Hem<p>
+    </body>
+    </html>
+"""
+
 
 @app.route("/")
 def home():
-    return "<h1>Home<h1/>"
+    return page
 
 @app.route("/login")
 def login():
@@ -11,7 +24,8 @@ def login():
 
 @app.route("/logout")
 def logout():
-    return render_template('logout.html')
+    return render_template(logout.html)
+
 
 
 if __name__ == "__main__":
