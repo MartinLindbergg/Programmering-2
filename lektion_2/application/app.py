d4e712731e10f9cd0cc75ece1a410c5d6166c63e
Martin Lindberg <Martinlindberg@live.se>
2023-10-17 00:40:57 +0200

    update 231017

---
+++ lektion_2/application/app.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+page = """
+    <html>
+    <head><title>Hem</title></head>
+    <body>
+    <h1>Titel</h1>
+    <p>Detta är Hem<p>
+    </body>
+    </html>
+"""
+
+
+@app.route("/")
+def home():
+    return page
+
+@app.route("/login")
+def login():
+    return "<h1>LogIn<h1/>"
+
+@app.route("/logout")
+def logout():
+    return render_template(logout.html)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+#GÅR INTE ATT INSTALLERA MYSQL WHYYYY?
+#GÅR INTE ATT HÄMTA HTMLFILEN MED RENDER_TEMPLATE WHYYYY?
